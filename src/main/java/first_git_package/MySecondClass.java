fab6c7e970213fea4224da1635ae8e9519918d15
durmusmustafa <techpromustafa@gmail.com>
2023-03-12 19:01:50 +0100

    updating my second class

--- src/main/java/first_git_package/MySecondClass.java
+++ src/main/java/first_git_package/MySecondClass.java
@@ -3,5 +3,6 @@ package first_git_package;
 public class MySecondClass {
     public static void main(String[] args) {
         System.out.println("my second class");
+        System.out.println("updating existing code");
     }
 }
