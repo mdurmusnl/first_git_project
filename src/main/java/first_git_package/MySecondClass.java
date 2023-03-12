6bca75aaf7f7d1ba0e6b63f64d1e95e0207e2671
durmusmustafa <techpromustafa@gmail.com>
2023-03-12 18:48:55 +0100

    added my second java class

--- src/main/java/first_git_package/MySecondClass.java
+++ src/main/java/first_git_package/MySecondClass.java
@@ -2,6 +2,6 @@ package first_git_package;
 
 public class MySecondClass {
     public static void main(String[] args) {
-        System.out.println("this is my second class");
+        System.out.println("my second class");
     }
 }
